91c70667fa8e090bfe4e0442cb9600c8e74e95c2
davidlaughton11 <47399940+davidlaughton11@users.noreply.github.com>
2019-03-22 09:01:19 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _312576checkerboard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int j = 0; j < 8; j++)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    Rectangle w = new Rectangle();
+                    w.Height = 100;
+                    w.Width = 100;
+                    if ((j + i) % 2 == 0)
+                    {
+                        w.Fill = Brushes.Yellow;
+                    }
+                    else
+                    {
+                        w.Fill = Brushes.Black;
+                    }
+                    canvas.Children.Add(w);
+                    Canvas.SetTop(w, i * 100);
+                    Canvas.SetLeft(w, j * 100);
+                }
+            }
+        }
+
+    }
+}
+
